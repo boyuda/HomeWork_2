c4a3da8a6f6842c5606b0e58b9f3e9ad469f6342
Dmitrijus Byckovas <boyuda96@gmail.com>
2021-11-27 01:16:07 +0200

    Add project files.

---
+++ Task 2/Calculations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Calculations
+    {
+        public static List<String> WorkingWithList(String input)
+        {
+            var counter = 10;
+            var modulusNumber = 11;
+            int sumOfMultiplications = 0;
+
+            //Adding each digit to the list
+            var stringList = new List<String>();
+
+            foreach (char c in input)
+            {
+                stringList.Add(c.ToString());
+            }
+
+            //Finding Check Digit
+            for (int i = 0; i < stringList.Count; i++)
+            {
+                var multiplication = Convert.ToInt32(stringList[i]) * counter;
+                counter--;
+                sumOfMultiplications += multiplication;
+            }
+
+            var remainder = sumOfMultiplications % modulusNumber;
+            var checkDigit = modulusNumber - remainder;
+
+            //Applying condition to checkDigit output
+            if (checkDigit == 10)
+            {
+                stringList.Add("X");
+            }
+            else
+            {
+                stringList.Add(Convert.ToString(checkDigit));
+            }
+
+            return stringList;
+        }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+
+            Console.WriteLine("Please enter 9 numbers without any separation:");
+            input = Console.ReadLine();
+
+            Console.WriteLine(string.Format("ISBN: {0}", string.Join("", Calculations.WorkingWithList(input))));
+
+        }
+    }
+}
